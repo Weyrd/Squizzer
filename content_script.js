e477c39aa7ff641cdb7cdc986e4f7a8577aba39f
Weyrd (Pierre) <44951922+Weyrd@users.noreply.github.com>
2024-07-31 18:29:34 +0200

    Fix: Ne pas submit "requete en cours..." ou any others API messages

--- content_script.js
+++ content_script.js
@@ -4,21 +4,23 @@ const MESSAGES = {
   CODE_ERROR: "Erreur dans le code: ",
   EMPTY_RESPONSE: "La réponse est vide.",
   RESPONSE_RECEIVED: "Réponse : ",
-  WAITING_FOR_QUESTION: "En attente d'une question...",
+  WAITING_FOR_QUESTION: "En attente d'une nouvelle question...",
   GAME_STARTING: "La partie va bientôt commencer",
+  I_DONT_KNOW: "Je ne sais pas",
 };
 
 const PROMPT = `Tu es un super champion de Question Pour un champion. 
 Tu as gagné des centaines de fois et tu es la personne la plus cultivée dans tous les differents domaines au monde. 
 Pour ce quizz tu dois suivre des règles strictes.
 - Pas de ponctuations, ni de majuscules ni d'accent.
-- Si on te demande un nombre, réponds toujours en chiffres sans texte (example: écrit "6" pas "six"). Parfois tu devras rajouter "ans", "mètres" ou l'unité à la fin.
+- Si on te demande un nombre, réponds toujours en chiffres sans texte (example: écrit "6" pas "six"). Parfois tu devras rajouter "ans", "mètres" ou l'unité à la fin. 
+    Si l'unité est déjà présente dans la questions, ne pas la rajouter dans la réponse.
 - Répond en 5 mots maximum.
 - Si tu peux répondre en un seul mot, c'est encore mieux.
-- Si tu ne connais pas la réponse, réponds "Je ne sais pas".
 - Si tu dois compléter une phrase, réponds uniquement avec le ou les mots manquant. Ne pas réécrire la phrase entière.
-    (Example : Quand on est malade on à une fièvre de... Réponds "cheval")
+  (Example : Quand on est malade on à une fièvre de... Réponds "cheval")
 - Si on te demande des questions sur des titres japonais, essaye de trouver le titrer francais si possible. Sinon réponds romaji.
+- Si tu ne connais pas la réponse, réponds ${MESSAGES.I_DONT_KNOW}.
 `;
 
 const OPENAI_API_KEY =
@@ -188,8 +190,8 @@ function start() {
 
 function stop() {
   console.log("Stoping the script");
-  document.body.style.border = "none";
   removeObserver();
+  document.body.style.border = "none";
   if (document.querySelector("#divGPT")) document.querySelector("#divGPT").remove();
 }
 
@@ -242,7 +244,7 @@ function simulateTyping(input, text, delay, pressEnter) {
 // insertAnswerGPT
 function insertAnswerGPT(e) {
   var answerGPT = divTextAnswerGPT.innerText.replace(MESSAGES.RESPONSE_RECEIVED, "").trim();
-  if (answerGPT != "") {
+  if (answerGPT != "" && !Object.values(MESSAGES).includes(answerGPT)) {
     input = getElementByXpath(INPUT_XPATH);
     input.focus();
 
