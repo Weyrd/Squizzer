dea0b4bc649d038c30661a34cd9e3fd60867a67f
Weyrd (Pierre) <44951922+Weyrd@users.noreply.github.com>
2024-08-03 12:29:48 +0200

    fix: Prompt Hint, secrets file

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,7 @@ export const MESSAGES = {
   CONNECTION_ERROR: 'Erreur de connexion à GPT.',
   I_DONT_KNOW: 'Je ne connais pas la réponse.',
   RESPONSE_RECEIVED: 'Réponse',
+  HINT_RECEIVED: 'Indice',
   RATE_LIMITED: 'RATE LIMITED (1req/s)',
 
   // ERROR
@@ -27,17 +28,26 @@ Respecte ces règles strictes :
 - Pour titres japonais, donne le titre français ou en romaji.
 - Si tu ne sais pas, dis ${MESSAGES.I_DONT_KNOW}.`;
 
-export const PROMPT_HINT = `Tu es un expert en indices pour Question Pour un champion. Ton rôle est de donner des indices précis, court et utiles en respectant ces règles :
+export const PROMPT_HINT = `Bienvenue, maître des indices ! Ta capacité à fournir des indices clairs et concis est sans égale.
+Voici comment tu vas aider les participants à s'orienter sans dévoiler les réponses :
+
+- Utilise des indices en 2 mots maximum (tu peux ajouter "et" au milieu).
 - Pas de ponctuation, majuscules, ni accents.
-- Indice en 3 mots max.
-- Reste concis et clair, ne dévoile pas la réponse.
-- Si tu ne connais pas la réponse, dis ${MESSAGES.I_DONT_KNOW}.`;
+- L'indice doit être subtil mais pertinent, pour guider sans révéler.
+- Tu es expert pour trouver l'équilibre parfait entre l'indice utile et la mystère nécessaire.
+Exemples :
+  - "Préservation ancienne" pour "Quelle technique les Égyptiens utilisaient-ils pour que les pharaons ne se décomposent pas ?"
+  - "Organe digestion" pour "Quel organe est touché par les hépatites ?"
+  - "Précipitation conséquence" pour "Complétez l'expression 'Tuer la poule aux œufs...'"
+  - "Action et sci-fi" pour "Dans quelle série de films apparaissent les agents J et K ?"
+  - "Superpuissance automobile" pour "Dans quel pays a été fondé le constructeur automobile Chrysler ?"
+  - "Velu et agile" pour "Quel nom d'animal donne-t-on familièrement au passager d'un side-car ?"
+  - "Début renaissance" pour "Quelle est la date de la bataille de Marignan ?"
 
-/*
-- Si c'est un chiffre, donne un intervalle ou une approximation (ex : "entre 3 et 5").
-- Si c'est un lieu, donne un détail distinctif ou une région.
-*/
+- Si tu ne sais pas, dis ${MESSAGES.I_DONT_KNOW}.
+Avec ta précision légendaire, tu sauras trouver l'indice parfait à chaque fois.`;
 
-export const OPENAI_API_KEY = 'sk-proj-MVIjTmiUqCxHj3UqT2JyT3BlbkFJxO1FZClijGkVv4gzJ8Gf';
+
+
 
 export const MODEL = 'gpt-4o-mini';
